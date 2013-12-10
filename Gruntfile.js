7f3a3bf40296a1317273d8d2c1ffcdd520773673
Daniel Freedman <dfreedm@google.com>
2013-12-09 17:20:14 -0800

    Fix tests
    - Expected files were created in wrong path
    - Added grunt-mkdir to make the tmp folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,15 @@ module.exports = function(grunt) {
       tests: ['tmp'],
     },
 
+    // set up tmp folders
+    mkdir: {
+      all: {
+        options: {
+          create: ['tmp/default', 'tmp/csp', 'tmp/inline', 'tmp/excludes']
+        }
+      }
+    },
+
     // Configuration to be run (and then tested).
     vulcanize: {
       default: {
@@ -78,11 +87,12 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-mkdir');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'vulcanize', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'mkdir', 'vulcanize', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
