6941ac0455ab5e6c22f48012fd96f76763621b8d
Daniel Freedman <dfreedm@google.com>
2014-03-31 19:50:14 -0700

    update to use vulcanize 0.2

--- tasks/vulcanize.js
+++ tasks/vulcanize.js
@@ -14,14 +14,26 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
+  //TODO: fix this in vulcanize
+  function excludeFixup(sink, source) {
+    if (source) {
+      source.forEach(function(s) {
+        sink.push(new RegExp(s));
+      });
+    }
+  }
+
   grunt.registerMultiTask('vulcanize', 'Inline HTML Imports', function() {
+    var done = this.async();
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       csp: false,
       inline: false,
       strip: false,
       excludes: {
-        imports: []
+        imports: [],
+        scripts: [],
+        styles: []
       }
     });
 
@@ -42,13 +54,20 @@ module.exports = function(grunt) {
       options.input = src[0];
       options.output = f.dest;
 
-      if (vulcanize.setOptions(options)) {
+      var e = options.excludes;
+
+      vulcanize.setOptions(options, function(err) {
+        if (err) {
+          return grunt.fatal(err);
+        }
+        excludeFixup(options.excludes.imports, e.imports);
+        excludeFixup(options.excludes.styles, e.styles);
+        excludeFixup(options.excludes.scripts, e.scripts);
         vulcanize.processDocument();
         grunt.log.ok();
         grunt.verbose.writeln('wrote %s', f.dest + (!options.csp ? '' : ' and ' + f.dest.replace('.html', '.js')));
-      } else {
-        grunt.fatal('No file given to vulcanize!');
-      }
+        done();
+      });
 
     });
   });
