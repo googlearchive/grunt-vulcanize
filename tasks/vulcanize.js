de2f27e1be8287d264977b4d743328d02e6ae5b7
Daniel Freedman <dfreedm2@gmail.com>
2014-04-02 10:45:55 -0700

    Rebaseline excludes test
    Update to vulcanize 0.2.4

--- tasks/vulcanize.js
+++ tasks/vulcanize.js
@@ -14,19 +14,11 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  //TODO: fix this in vulcanize
-  function excludeFixup(sink, source) {
-    if (source) {
-      source.forEach(function(s) {
-        sink.push(new RegExp(s));
-      });
-    }
-  }
-
   grunt.registerMultiTask('vulcanize', 'Inline HTML Imports', function() {
     var done = this.async();
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
+      config: null,
       csp: false,
       inline: false,
       strip: false,
@@ -54,15 +46,10 @@ module.exports = function(grunt) {
       options.input = src[0];
       options.output = f.dest;
 
-      var e = options.excludes;
-
       vulcanize.setOptions(options, function(err) {
         if (err) {
           return grunt.fatal(err);
         }
-        excludeFixup(options.excludes.imports, e.imports);
-        excludeFixup(options.excludes.styles, e.styles);
-        excludeFixup(options.excludes.scripts, e.scripts);
         vulcanize.processDocument();
         grunt.log.ok();
         grunt.verbose.writeln('wrote %s', f.dest + (!options.csp ? '' : ' and ' + f.dest.replace('.html', '.js')));
